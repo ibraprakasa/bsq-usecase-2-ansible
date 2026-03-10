63d7410d486d077b2318ef3996fb29352cdbd80c
Ibra Prakasa <113502572+ibraprakasa@users.noreply.github.com>
2026-03-10 13:43:35 +0700

    Update gcp_alert_listener.py

--- rulebooks/gcp_alert_listener.py
+++ rulebooks/gcp_alert_listener.py
@@ -1,7 +1,23 @@
 import json
 import time
+from google.cloud import monitoring_v3
+
+project_id = "orms-sta-project"
+
+client = monitoring_v3.AlertPolicyServiceClient()
+name = f"projects/{project_id}"
 
 while True:
-    event = {"alert": "test"}
-    print(json.dumps(event), flush=True)
-    time.sleep(30)
+
+    policies = client.list_alert_policies(request={"name": name})
+
+    for p in policies:
+
+        event = {
+            "alert_name": p.display_name,
+            "project": project_id
+        }
+
+        print(json.dumps(event), flush=True)
+
+    time.sleep(60)
